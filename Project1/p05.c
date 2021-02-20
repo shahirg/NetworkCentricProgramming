ebed07c4998095673e34caacd5720371b7fcee3b
shahirg <43625705+shahirg@users.noreply.github.com>
2021-02-19 21:41:13 -0500

    update structure

--- Project1/p05.c
+++ Project1/p05.c
@@ -5,25 +5,28 @@
 #include <ctype.h>
 #include <errno.h>
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 	// min number of for argc = 3
 	// exe <filename> <substringstring>
-	if(argc <3) {
+	if (argc < 3)
+	{
 		perror("Not enough input arguements\n");
 		exit(1);
 	}
 	//check flag
-	int flag = strcmp(argv[1], "--systemcalls") == 0 ? 1:0;
+	int flag = strcmp(argv[1], "--systemcalls") == 0 ? 1 : 0;
 
-	//min argc value if systemcall is 4 because you need file and 
-	if(flag && argc < 4) {
+	//min argc value if systemcall is 4 because you need file and
+	if (flag && argc < 4)
+	{
 		perror("Not enough input arguements\n");
 		exit(1);
 	}
 
 	//printf("%d",argc);
 	//number of string is dependent on weather or not there is a flag
-	int numSubstrings = flag ? (argc - 3):(argc -2);
+	int numSubstrings = flag ? (argc - 3) : (argc - 2);
 
 	//create buffer for substrings
 	//char* strBuffer=(char*) malloc (sizeof(char)*numSubstrings);
@@ -32,13 +35,14 @@ int main(int argc, char* argv[]){
 	//	//get substrings based on weather or not flag
 	//	char* str = flag ? argv[i + 3]:argv[i + 2];
 	//	//lower case string to make case insensitive
-	//	for(char *p=str; *p; p++) 
+	//	for(char *p=str; *p; p++)
 	//		*p = tolower(*p);
 	//	strBuffer[i] = str;
 	//}
 
-	if(!flag){
-		
+	if (!flag)
+	{
+
 		//check if file exists
 		// if(access(argv[1], F_OK) != 0){
 		// 	printf("File does not exist");
@@ -48,42 +52,43 @@ int main(int argc, char* argv[]){
 
 		//pointer to file
 		FILE *pFile;
-		size_t sz = 0; 
-		ssize_t len; 
-		char* line = NULL; //line
-
+		size_t sz = 0;
+		ssize_t len;
+		char *line = NULL; //line
 
 		//open the file
-		pFile = fopen(argv[1],"rb");
-		if(pFile==NULL){
+		pFile = fopen(argv[1], "rb");
+		if (pFile == NULL)
+		{
 			perror("Reading error\n");
 			exit(1);
 		}
 
 		//for each line in the file...
-		int counter;
-		for(int i=0; i<(argc-2);i++) {   		
-			counter=0;
-			sz = 0;line = NULL;
-			while((len = getline(&line, &sz, pFile)) != -1) {
+		int *counter = (int *)malloc(numSubstrings * sizeof(int));
+		while ((len = getline(&line, &sz, pFile)) != -1)
+		{
+
+			for (int i = 0; i < (numSubstrings); i++)
+			{
 
 				printf("sdfjk\n");
-				int substrlen=strlen(argv[i+2]);
-				for(int j=0;j<(strlen(line)-substrlen+1);j++)  {
-					char * substrofbuffer=(char*) malloc (sizeof(char)*substrlen);
-					strncpy(substrofbuffer,line+j,substrlen);
-					if(strcasecmp(substrofbuffer,argv[i+2])==0) {
-						counter++;
+				int substrlen = strlen(argv[i + 2]);
+				for (int j = 0; j < (strlen(line) - substrlen + 1); j++)
+				{
+					char *substrofbuffer = (char *)malloc(sizeof(char) * substrlen);
+					strncpy(substrofbuffer, line + j, substrlen);
+					if (strcasecmp(substrofbuffer, argv[i + 2]) == 0)
+					{
+						counter[i] += 1;
 					}
 				}
-			
 			}
-			
-			fclose(pFile);	
+
 			printf("test\n");
-			printf("%d\n",counter);
+			printf("%d\n", counter);
 			// //for each substring passed in
-			pFile = fopen(argv[1],"rb");
+
 			// for(int i = 0; i < size; i++){
 
 			// //get the substring
@@ -104,26 +109,22 @@ int main(int argc, char* argv[]){
 			// count = 0;
 
 			// }
-
-
 		}
 		fclose(pFile);
 
-		free(line);		
-
-
-			
+		free(line);
 	}
-	else { //flag true
+	else
+	{	//flag true
 		//code
 	}
-	
+
 	return 0;
 }
 
 // FILE *pFile;
 // long lSize;
-// char*buffer; 
+// char*buffer;
 // size_t read;
 
 // //check file err

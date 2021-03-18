2a5cd16802b7bc83591772363aba8ea80f56c8f5
shahirg <shahir13579@gmail.com>
2021-03-18 19:57:55 -0400

    one process working

--- Homework1/sum.c
+++ Homework1/sum.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char** argv){
-    
+int sum(int start, int end, int total) {
+    return start == end ? total + start : sum(start + 1, end, total + start);
+}
+int main(int argc, char **argv) {
+    printf("%d\n", sum(1, 100, 0));
     return 0;
 }

